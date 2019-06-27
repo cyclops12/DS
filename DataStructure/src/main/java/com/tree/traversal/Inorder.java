2676bf3f06ce3c09232e833acba624d088b504c0
Ratan <Ratan@LAPTOP-4OP23DM7>
2019-06-27 20:47:02 +0530

    initial

---
+++ DataStructure/src/main/java/com/tree/traversal/Inorder.java
@@ -0,0 +1,16 @@
+package com.tree.traversal;
+
+import com.tree.Node;
+
+/**
+ * L Root Right
+ * @author Ratan
+ *
+ */
+public class Inorder {
+	
+	public static void traverse(Node node) {
+		
+	}
+
+}
